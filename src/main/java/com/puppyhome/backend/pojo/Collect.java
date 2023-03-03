b0d5e0117415949f4c0b8e4f3f80c1a513de05fa
WA_automat <1577696824@qq.com>
2023-03-03 13:57:14 +0800

    create collect

---
+++ src/main/java/com/puppyhome/backend/pojo/Collect.java
@@ -0,0 +1,29 @@
+package com.puppyhome.backend.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("collect")
+public class Collect {
+
+	@TableId(type = IdType.AUTO)
+	private Integer id;
+
+	@NonNull
+	@TableField("userId")
+	private Integer userId;
+
+	@NonNull
+	@TableField("dogId")
+	private Integer dogId;
+
+}
