12f1383c349aad6ca7f8596f99da44d28322eb5e
WA_automat <1577696824@qq.com>
2023-03-15 15:49:28 +0800

    debug exception

--- src/main/java/com/puppyhome/backend/controller/ImagePredictController.java
+++ src/main/java/com/puppyhome/backend/controller/ImagePredictController.java
@@ -1,5 +1,8 @@
 package com.puppyhome.backend.controller;
 
+import ai.djl.MalformedModelException;
+import ai.djl.repository.zoo.ModelNotFoundException;
+import ai.djl.translate.TranslateException;
 import com.puppyhome.backend.service.predict.ImagePredictService;
 import com.puppyhome.backend.utils.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/image")
@@ -18,7 +23,7 @@ public class ImagePredictController {
 	@GetMapping("/predict")
 	public ResponseResult imagePredict(
 			@RequestParam("url") String url
-	){
+	) throws TranslateException, ModelNotFoundException, MalformedModelException, IOException {
 		return imagePredictService.imagePredict(url);
 	}
 
